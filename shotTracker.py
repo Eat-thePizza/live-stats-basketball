34221cc3e67ba39d2c498274a9b9d257ee28539a
Eat-thePizza <85592401+Eat-thePizza@users.noreply.github.com>
2026-03-16 16:36:53 -0700

    shotTracker

---
+++ shotTracker.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+#import intel_extension_for_pytorch as ipex
+import torch
+from ultralytics import YOLO
+from homographyEstimation import image_estimation
+
+video_path = "C:/Users/ethan/Downloads/GunnTestingTrim.mp4"
+
+cap = cv2.VideoCapture("C:/Users/ethan/Downloads/GunnTestingTrim.mp4")
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+frame_num = 0
+condition = True
+
+while condition:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    if frame_num == 157:
+        image_estimation(ret,frame,"testing")
+        condition = False
+    frame_num += 1
